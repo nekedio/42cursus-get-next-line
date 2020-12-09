27c6ef0c80240463cc0d6573d0fd46b9892f2203
Danielle Xenophilius <dxenophi@et-m5.kzn.21-school.ru>
2020-12-09 19:11:19 +0300

    modified get_next_line.h

--- get_next_line.h
+++ get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: dxenophi <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/11/26 18:32:42 by dxenophi          #+#    #+#             */
-/*   Updated: 2020/12/07 18:02:39 by dxenophi         ###   ########.fr       */
+/*   Updated: 2020/12/09 15:30:50 by dxenophi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,9 @@
 # include <stdlib.h>
 
 int		get_next_line(int fd, char **line);
-void	*ft_calloc(size_t count, size_t size);
 size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_strchr(const char *str, int chr);
 char	*ft_strjoin(char const *s_buf, char const *str2);
-char	*ft_strdup(const char *str);
 size_t	ft_strlen(const char *src);
 
 #endif
